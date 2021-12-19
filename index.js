6d47ec67b051369895d0e0854beb0bf484f83976
Hasan Nakib <hsnnkb.dev@gmail.com>
2021-12-19 21:33:26 +0000

    Remove unused code, Add comments to code, General tidying up of code

--- index.js
+++ index.js
@@ -1,48 +1,30 @@
-//document.getElementById("count-el").innerText = 10;
-
-// 1.
-// let myAge = 22;
-// 2.
-// console.log(myAge);
-
-// 3.
-// const humanDogRatio = 7;
-// let myDogAge = myAge * humanDogRatio;
-// console.log(myDogAge);
-
-// 4.
-// let bonusPoints = 50;
-// console.log(bonusPoints);
-// bonusPoints = 100;
-// console.log(bonusPoints);
-// bonusPoints = 25;
-// console.log(bonusPoints);
-// bonusPoints = 70;
-// console.log(bonusPoints);
-
+/* Variables */
 let count = 0;
-//console.log(count);
 let saveEl = document.getElementById("save-el");
 
+
+/* Functions */
+
+// Increases the counter by a step of 1
 function increment() {
-    //console.log("The button was clicked!")
     count += 1;
     document.getElementById("count-el").textContent = count;
 }
 
+// Decreases the counter by a step of 1
 function decrement() {
-    //console.log("The button was clicked!")
     count -= 1;
     document.getElementById("count-el").textContent = count;
 }
 
+// Store the current count in the "Previous Entries: " section
 function save() {
     countMessage = count + ", ";
     saveEl.textContent += countMessage;
-    //console.log(count);
     resetCounter();
 }
 
+// Reset the counter to 0
 function resetCounter() {
     count = 0;
     document.getElementById("count-el").textContent = count;
